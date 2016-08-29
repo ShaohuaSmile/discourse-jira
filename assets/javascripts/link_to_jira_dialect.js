169e05f377c5dd3889860dd4697b6db3f093af34
U-ERICSSON\esualiu <esualiu@CN00202765.ericsson.se>
2016-08-29 20:22:57 +0800

    update

--- assets/javascripts/link_to_jira_dialect.js
+++ assets/javascripts/link_to_jira_dialect.js
@@ -1,25 +1,21 @@
 (function() {
     if (Discourse.dialect_deprecated) { return; }
-    
     function link_to_jira(text) {
-		
         var siteSettings = Discourse.SiteSettings;
-
         if (siteSettings.link_to_jira_plugin_enabled) {
-        var list = siteSettings.link_to_jira_plugin_list;
-
-        var link_to_jira_projects = list.split("|");
-        var link_to_jira_issues = new Array(0);
-        for(var i=0; i < link_to_jira_projects.length; i++){
-		 //HHHHHJCAT-1234CCCCCC
-			var matchKey = link_to_jira_projects[i] + "-";
-			findIssue(link_to_jira_issues,text,matchKey,0);
-		}
-		for(var i=0; i < link_to_jira_issues.length; i++){
-		 text = text.replace(link_to_jira_issues[i], '<a href="' +'https://plf-jira.rnd.ki.sw.ericsson.se/browse/'+ link_to_jira_issues[i] + '">'+ link_to_jira_issues[i] +'</a>');
-		} 
+            var list = siteSettings.link_to_jira_plugin_list;
+            var link_to_jira_projects = list.split("|");
+            var link_to_jira_issues = new Array(0);
+            for(var i=0; i < link_to_jira_projects.length; i++){
+		        //HHHHHJCAT-1234CCCCCC
+			    var matchKey = link_to_jira_projects[i] + "-";
+			    findIssue(link_to_jira_issues,text,matchKey,0);
+		    }
+		    for(var i=0; i < link_to_jira_issues.length; i++){
+		        text = text.replace(link_to_jira_issues[i], '<a href="' +'https://plf-jira.rnd.ki.sw.ericsson.se/browse/'+ link_to_jira_issues[i] + '">'+ link_to_jira_issues[i] +'</a>');
+		    } 
         }
-    return text;
+        return text;
     }
     function findIssue(issues, str, matchKey, beginIndex){
 		var index = str.indexOf(matchKey, beginIndex);
@@ -30,14 +26,16 @@
 		var i = index + matchKey.length;
 		while(true){
 			var c = str.charAt(i++);
-			if(!isNaN(c)){
-				issue = issue + c;
-			}else{
+			//not a number or empty or undefined
+			if(isNaN(c) || "" == c || undefined == c){
 				break;
-			}			
+			}
+			issue = issue + c;
 		}
-		issues.push(issue);		
-		findIssue(issues, str, matchKey, index + i); 	 		
+		if(issue != matchKey){
+		    issues.push(issue);
+		}
+		return findIssue(issues, str, matchKey, index + 1);
 	}
 
     Discourse.Dialect.addPreProcessor(function(text) {
