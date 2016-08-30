709af0ef45564a9e040bfbdd3b83e50332f91ca1
U-ERICSSON\esualiu <esualiu@CN00202765.ericsson.se>
2016-08-30 20:15:10 +0800

    add by xuanren

--- assets/javascripts/link_to_jira_dialect.js
+++ assets/javascripts/link_to_jira_dialect.js
@@ -1,47 +1,83 @@
-(function() {
-    if (Discourse.dialect_deprecated) { return; }
-    function link_to_jira(text) {
-        var siteSettings = Discourse.SiteSettings;
-        if (siteSettings.link_to_jira_plugin_enabled) {
-            var list = siteSettings.link_to_jira_plugin_list;
-            var link_to_jira_projects = list.split("|");
-            var link_to_jira_issues = new Array(0);
-            for(var i=0; i < link_to_jira_projects.length; i++){
-		        //HHHHHJCAT-1234CCCCCC
-			    var matchKey = link_to_jira_projects[i] + "-";
-			    findIssue(link_to_jira_issues,text,matchKey,0);
-		    }
-		    for(var i=0; i < link_to_jira_issues.length; i++){
-		        text = text.replace(link_to_jira_issues[i], '<a href="' +'https://plf-jira.rnd.ki.sw.ericsson.se/browse/'+ link_to_jira_issues[i] + '">'+ link_to_jira_issues[i] +'</a>');
-		    } 
-        }
-        return text;
-    }
-    function findIssue(issues, str, matchKey, beginIndex){
-		var index = str.indexOf(matchKey, beginIndex);
-		if(index < 0){
-			return issues;
-		}
-		if(index != 0 && (str.charAt(index-1) == '\/' || str.charAt(index-1) == '>')){
-			return findIssue(issues, str, matchKey, index + 1);
-		}
-		var issue = matchKey;
-		var i = index + matchKey.length;
-		while(true){
-			var c = str.charAt(i++);
-			//not a number or empty or undefined
-			if(isNaN(c) || '' == c || ' ' == c || "" == c || " " == c || undefined == c){
-				break;
-			}
-			issue = issue + c;
-		}
-		if(issue != matchKey){
-		    issues.push(issue);
-		}
-		return findIssue(issues, str, matchKey, index + 1);
-	}
 
-    Discourse.Dialect.addPreProcessor(function(text) {
-        return link_to_jira(text);
+function addHref(text,regexStr,keys,values) {
+
+    regexStr = regexStr.replace(/([^|]+)/gm,"$1\\s*-\\s*\\d{1,5}");
+    regexStr = "(" + regexStr + ")";
+    var regex = new RegExp( regexStr ,"gm");
+
+    return replaceProcessor("tag",regex,text);
+
+    function getHrefPrefix(match) {
+        var key = match.substring(0,match.indexOf("-"));
+        for ( var i = 0; i < keys.length; i++){
+            if ( keys[i] == key){
+                return values[i];
+            }
+        }
+    }
+    function replaceProcessor(seperatorType,targetRegex,text) {
+        var urlPatten = /https?:\/\/[^<> ()]+|www.[^<> ()]*/gm;
+        var tagPatten = /<[^>]*[^<]*>/gm;
+        var seperatorRegex;
+        var result = new Array();
+        var targetStr ;
+        var subResult ;
+        var lastLocation = 0;
+
+        if(seperatorType != "null" ) {
+            if ( seperatorType == "tag"){
+                seperatorRegex = tagPatten;
+            }else {
+                seperatorRegex = urlPatten;
+            }
+            var matches = seperatorRegex.exec(text);
+            while (matches != null){
+                targetStr = text.substring(lastLocation,matches.index);
+                if (targetStr.length != 0){
+                    if ( seperatorType == "tag"){
+                        subResult = replaceProcessor("url",targetRegex,targetStr);
+                    }else {
+                        subResult = replaceProcessor("null",targetRegex,targetStr);
+                    }
+                    result += subResult;
+                }
+                result +=  matches[0];
+                lastLocation = seperatorRegex.lastIndex;
+                matches = seperatorRegex.exec(text);
+            }
+        }
+
+        targetStr = text.substring(lastLocation);
+        if ( seperatorType == "tag"){
+            subResult = replaceProcessor("url",targetRegex,targetStr);
+        }else if (seperatorType == "url"){
+            subResult = replaceProcessor("null",targetRegex,targetStr);
+        }else {
+            subResult = targetStr.replace(regex,function (match) {
+                return "<a href=\"" + getHrefPrefix(match) + match + "\">" + match + "</a>";
+            });
+        }
+
+        return result + subResult;
+    }
+}
+function link_to_jira(text) {
+     var siteSettings = Discourse.SiteSettings;
+    
+     if (siteSettings.abbreviations_plugin_enabled) {
+         var list = siteSettings.abbreviations_plugin_list;
+    var regexStr = new String();
+    var keys = new Array();
+    var values = new Array();
+    var start;
+    list.trim().split("|").map(function(schema) {
+        start = schema.indexOf(":");
+        keys.push(schema.substring(0,start));
+        values.push(schema.substring(start + 1, schema.length));
+        regexStr += schema.substring(0,start) + "|";
     });
-})();
+    regexStr = regexStr.substring(0,regexStr.length - 1) ;
+    return addHref(text,regexStr,keys,values);
+
+     }
+}
